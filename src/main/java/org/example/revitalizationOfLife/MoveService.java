5c057664d51a91006b15bc98cad6353475b4beac
Dan Dan <daniilhlysemkov@gmail.com>
2026-07-28 14:25:22 +0300

    Закончил MoveService добавил Direction.java чтобы локанизировать сервис и сделать его леггко читаемым

--- src/main/java/org/example/revitalizationOfLife/MoveService.java
+++ src/main/java/org/example/revitalizationOfLife/MoveService.java
@@ -7,18 +7,30 @@ import org.example.islandManager.Coordinate;
 import org.example.population.Animals;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class MoveService {
     private final Island island;
     private final EvanteLogger track;
 
+
     public MoveService(Island island, EvanteLogger track) {
         this.island = island;
         this.track = track;
     }
 
-    public void procces() {
+    private List<Direction> getAvailableDirections(int x, int y) {
+        List<Direction> directions = new ArrayList<>();
+       for (Direction direction : Direction.values()) {
+           if (island.isAvailable(x + direction.dx, y + direction.dy)) {
+               directions.add(direction);
+           }
+       }
+        return directions;
+    }
+
+    public void process() {
         for (Cage cage : island.getCages()) {
             new ArrayList<>(cage.getAnimals())
                     .forEach(animal -> move(animal, cage));
@@ -26,27 +38,19 @@ public class MoveService {
     }
 
     private void move(Animals animal, Cage cage) {
-        int x = cage.getCoordinate().getX();
-        int y = cage.getCoordinate().getY();
-
-        int direction = new Random().nextInt(4);
-
+        int curentX = cage.getCoordinate().getX();
+        int curentY = cage.getCoordinate().getY();
+        List<Direction> currentDirections = getAvailableDirections(curentX, curentY);
+        Direction direction = currentDirections.get(new Random().nextInt(currentDirections.size()));
         for (int i = 0; i < animal.getAnimalsType().speed; i++) {
-            int newX = x;
-            int newY = y;
-
-            if (direction == 0) newY--;      // вверх
-            else if (direction == 1) newY++; // вниз
-            else if (direction == 2) newX--; // влево
-            else newX++;                     // вправо
-
+            int newX = curentX +  direction.dx;
+            int newY = curentY  +  direction.dy;
             if (island.isAvailable(newX, newY)) {
-                x = newX;
-                y = newY;
+                curentX = newX;
+                curentY = newY;
             }
         }
-
-        Cage newCage = island.getCage(new Coordinate(x, y));
+        Cage newCage = island.getCage(new Coordinate(curentX, curentY));
         if (newCage != cage) {
             track.log(animal.getAnimalsType().symbol + " moved from"
                     + cage.getCoordinate().getX() + "," + cage.getCoordinate().getY()
@@ -55,4 +59,5 @@ public class MoveService {
             newCage.addAnimal(animal);
         }
     }
+
 }
